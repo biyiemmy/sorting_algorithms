84359def4fd173917eab0c5c71293cbf73bf45c5
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-11-08 01:18:49 -0800

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,9 +2,9 @@
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in ascending
- * order using the Insertion sort algorithm. It works similar to the way you 
- * sort playing cards in your hands.
+ * order using the Insertion sort algorithm
  * @list: Double pointer to the head of the linked list
+ *
  * Return: void
  */
 void insertion_sort_list(listint_t **list)
